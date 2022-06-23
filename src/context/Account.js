3c0ae9005903b21fc33a414fe48694c196255d23
Ryan Hong <65246381+ryan-hong@users.noreply.github.com>
2022-06-23 16:18:06 -0300

    redirect if user is not logged in

--- src/context/Account.js
+++ src/context/Account.js
@@ -1,73 +1,85 @@
-import * as React from 'react'
-import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js"
-import Pool from '../UserPool'
-import { useNavigate } from 'react-router-dom'
+import * as React from 'react';
+import { CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
+import Pool from '../UserPool';
+import { useNavigate } from 'react-router-dom';
 
-const AccountContext = React.createContext()
+const AccountContext = React.createContext();
 
 const Account = ({ children }) => {
   const nav = useNavigate();
 
   const getSession = async () => {
     return await new Promise((resolve, reject) => {
-      const user = Pool.getCurrentUser()
+      const user = Pool.getCurrentUser();
       if (user) {
         user.getSession((err, session) => {
           if (err) {
-            reject()
+            reject();
           } else {
-            resolve(session)
+            resolve(session);
+            console.log(session);
           }
-        })
+        });
       } else {
-        reject()
+        reject();
       }
-    })
-  }
+    });
+  };
 
   const authenticate = async (Username, Password) => {
     return await new Promise((resolve, reject) => {
       const user = new CognitoUser({
         Username,
-        Pool
-      })
+        Pool,
+      });
 
       const authDetails = new AuthenticationDetails({
         Username,
-        Password
-      })
+        Password,
+      });
 
       user.authenticateUser(authDetails, {
         onSuccess: (data) => {
-          console.log("onSuccess:", data)
-          resolve(data)
-          nav("/home")
+          console.log('onSuccess:', data);
+          resolve(data);
+          nav('/home', { state: { token: data.accessToken.jwtToken } });
         },
         onFailure: (err) => {
-          console.error("onFailure:", err)
-          reject(err)
+          console.error('onFailure:', err);
+          reject(err);
         },
         newPasswordRequired: (data) => {
-          console.log("newPasswordRequired:", data)
-          resolve(data)
-        }
-      })
-    })
-  }
+          console.log('newPasswordRequired:', data);
+          resolve(data);
+        },
+      });
+    });
+  };
 
   const logout = () => {
-    const user = Pool.getCurrentUser()
+    const user = Pool.getCurrentUser();
     if (user) {
-      user.signOut()
-      nav("/")
+      user.signOut();
+      nav('/');
     }
-  }
+  };
+
+  const isLoggedIn = () => {
+    const user = Pool.getCurrentUser();
+    console.log(user);
+    if (user !== undefined) {
+      return true;
+    }
+    return false;
+  };
 
   return (
-    <AccountContext.Provider value={{ authenticate, getSession, logout }}>
+    <AccountContext.Provider
+      value={{ authenticate, getSession, logout, isLoggedIn }}
+    >
       {children}
     </AccountContext.Provider>
-  )
-}
+  );
+};
 
-export { Account, AccountContext }
+export { Account, AccountContext };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,57 @@
-import * as React from 'react'
+import * as React from 'react';
 import { AccountContext } from '../context/Account';
+import { useLocation, Link } from 'react-router-dom';
 
 const Home = () => {
-  const { logout } = React.useContext(AccountContext)
+  const { logout } = React.useContext(AccountContext);
+  const location = useLocation();
+
+  function isLoggedIn() {
+    try {
+      if (location.state.token) {
+        return true;
+      }
+    } catch (error) {
+      return false;
+    }
+  }
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center pt-12 gap-y-8'>
-        <h2 className='text-3xl items-center font-semibold'>Your song of the day is:</h2>
-        <img className='h-64 w-64' src="https://i.scdn.co/image/ab67616d0000b273baf2a68126739ff553f2930a" alt="runway" />
-        <p className='text-gray-800 font-semibold'>Runaway by Kanye West</p>
-        <button className="px-2 py-1 bg-yellow-200 rounded cursor-pointer hover:bg-yellow-400" onClick={logout}><span className="font-semibold">Logout</span></button>
+      <div>
+        {isLoggedIn() ? (
+          <div className="flex flex-col items-center justify-center pt-12 gap-y-8">
+            <h2 className="text-3xl items-center font-semibold">
+              Your song of the day is:
+            </h2>
+            <img
+              className="h-64 w-64"
+              src="https://i.scdn.co/image/ab67616d0000b273baf2a68126739ff553f2930a"
+              alt="runway"
+            />
+            <p className="text-gray-800 font-semibold">Runaway by Kanye West</p>
+            <button
+              className="px-2 py-1 bg-yellow-200 rounded cursor-pointer hover:bg-yellow-400"
+              onClick={logout}
+            >
+              <span className="font-semibold">Logout</span>
+            </button>
+          </div>
+        ) : (
+          <div className="flex h-screen">
+            <div className="m-auto">
+              <h1 className="font-bold">
+                You do not have access to this page. Please login to continue
+              </h1>
+              <button className="w-full text-center bg-indigo-500 py-3 text-white rounded-sm hover:bg-indigo-700">
+                <Link to={'/'}>Login</Link>
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
