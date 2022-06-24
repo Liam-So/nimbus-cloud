5ade3ec12bc7c366e8b5c0a59204e6b027a2b480
Liam So <Liam.So@dal.ca>
2022-06-24 18:10:26 -0300

    Added connection to dynamoDB and a GET user endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json())
 
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1' });
+
+const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+
+// Sample get endpoint to test
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Nimbus CSCI 4145 Project')
+})
+
+// Get a specific user from dynamoDB
+app.get('/users', (req, res) => {
+  if (req.body?.id) {
+    const params = {
+      TableName: 'users',
+      Key: {
+        'id': { S: req.body.id }
+      },
+    }
+    ddb.getItem(params, function (err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data.Item);
+        res.status(200).send(data.Item)
+      }
+    });
+  }
 })
 
 app.listen(port, () => {
