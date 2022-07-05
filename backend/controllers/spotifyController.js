ec0ebac6b7f328aac96b60dcec286f5961875466
Liam So <Liam.So@dal.ca>
2022-07-04 23:05:53 -0300

    added auth exchange

--- backend/controllers/spotifyController.js
+++ backend/controllers/spotifyController.js
@@ -1,5 +1,27 @@
+const request = require('request'); // "Request" library
+require('dotenv').config()
+
+const client_id = process.env.CLIENT_ID
+const client_secret = process.env.CLIENT_SECRET
+
+
 const authenticateApp = (req, res) => {
-  res.status(200).send("Well done")
+  const authOptions = {
+    url: process.env.SPOTIFY_API_URL,
+    headers: {
+      'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  request.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      res.status(200).send(body)
+    }
+  });
 }
 
 
--- backend/routes/spotifyRoutes.js
+++ backend/routes/spotifyRoutes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const spotifyController = require("../controllers/spotifyController")
 
-
 // Post a new user
 router.post('/', spotifyController.authenticateApp)
 
