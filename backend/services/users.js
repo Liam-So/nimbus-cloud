5feb8e6491d18a0ccebb982aa2ad2ec4e8a752e9
Liam So <Liam.So@dal.ca>
2022-07-12 22:21:15 -0300

    update all users with new songs

--- backend/services/users.js
+++ backend/services/users.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
 const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
+
 const getUserService = async (id) => {
   const params = {
     TableName: 'users',
@@ -19,6 +20,39 @@ const getAllUserService = async () => {
 };
 
 
+const updateUserSongOfDay = async ({ id, url, song, img, artists }) => {
+  const artistParams = artists.map((artist) => ({ S: artist }));
+  const params = {
+    ExpressionAttributeNames: {
+      "#SURL": "song_url",
+      "#SN": "song_name",
+      "#IG": "song_img",
+      "#A": "artists"
+    },
+    ExpressionAttributeValues: {
+      ":u": {
+        S: url
+      },
+      ":n": {
+        S: song
+      },
+      ":i": {
+        S: img
+      },
+      ":a": {
+        L: artistParams
+      }
+    },
+    Key: { id: { S: id } },
+    ReturnValues: "ALL_NEW",
+    TableName: "users",
+    UpdateExpression: "SET #SURL = :u, #SN = :n, #IG = :i, #A = :a"
+  };
+
+  return await ddb.updateItem(params).promise()
+}
+
+
 const postUserService = async (id, genres) => {
   const genreParams = genres.map((genre) => ({ S: genre }));
   const params = {
@@ -32,8 +66,10 @@ const postUserService = async (id, genres) => {
   return await ddb.putItem(params).promise();
 };
 
+
 module.exports = {
   getUserService,
   postUserService,
-  getAllUserService
+  getAllUserService,
+  updateUserSongOfDay
 };
