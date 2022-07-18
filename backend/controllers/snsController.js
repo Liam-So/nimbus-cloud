0e860b4698758702147fd82916c4d6a8d60d1650
Liam So <Liam.So@dal.ca>
2022-07-17 21:34:09 -0300

    another refactor

--- backend/controllers/snsController.js
+++ backend/controllers/snsController.js
@@ -1,20 +1,9 @@
-const AWS = require('aws-sdk');
-const sns = new AWS.SNS();
+const { getSandboxPhoneNumbers } = require("../services/sns");
 
-const getNumbers = (req, res) => {
+const getNumbers = async (req, res) => {
   try {
-    sns.listSMSSandboxPhoneNumbers(function (err, data) {
-      if (err) {
-        console.log(err, err.stack);
-      } else {
-        Object.entries(data.PhoneNumbers).map(([key, value]) => {
-          console.log(data.PhoneNumbers[key].PhoneNumber);
-        });
-        res.send({
-          'Phone Numbers': data.PhoneNumbers,
-        });
-      }
-    });
+    const numbers = await getSandboxPhoneNumbers();
+    res.send(numbers.PhoneNumbers)
   } catch (err) {
     res.send({ Error: 'An error has occurred' });
   }
